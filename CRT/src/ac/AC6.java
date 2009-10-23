685942a17b813df69ba295f8fb4c9083db600634
Olivier FINOT <Olivier FINOT@5b063238-cacf-11de-a774-41d3a1f294e1>
2009-10-23 16:24:14 +0000

    AC6 started not much but started anyway.

--- CRT/src/ac/AC6.java
+++ CRT/src/ac/AC6.java
@@ -1,5 +1,35 @@
 package ac;
 
+import java.util.ArrayList;
+
+import model.Model;
+import model.Variable;
+
 public class AC6 {
+	private Model model;
+	private ArrayList<Variable> variables;
 	
+	public AC6(Model m){
+		model = m;
+		variables = m.getVariables();
+	}
+	
+	private boolean nextSupport(Variable i, Variable j, int a, Integer b){
+		boolean res;
+		int tmp = b.intValue();
+		
+		if(tmp <= j.getDomain().last())
+		{
+			res = false;
+			while(!j.getDomain().contains(tmp)){
+				tmp++;
+			}
+		}
+		else
+		{
+			res = true;
+		}
+		b = new Integer(tmp);
+		return res;
+	}
 }
