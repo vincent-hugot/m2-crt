70e23233f90a0dcbd71bd9fe86db86727dd3b943
Vincent HUGOT <Vincent HUGOT@5b063238-cacf-11de-a774-41d3a1f294e1>
2009-10-26 15:33:24 +0000

    Parser: added comments

--- CRT/src/main/Main.java
+++ CRT/src/main/Main.java
@@ -11,6 +11,7 @@ public class Main {
 		String file = "src/parser/test.txt";
 		Translator tr = new Translator(file);
 		Model m = tr.translate();
+		tr.dumpErrors();
 		
 		System.out.println(m);
 	}
